0200ac7fd1a66a21aca33cfaff9c2ba3c1c2b77f
MrZinken <kaimecki@gmail.com>
2024-03-15 12:33:20 +0100

    run3

--- training/train.py
+++ training/train.py
@@ -5,7 +5,7 @@ model = YOLO("yolov8m-seg.pt")
 results = model.train(
         batch=20,
         device="cuda",
-        data="/home/kai/Documents/solar/v7/data.yaml",
+        data="/home/kai/Documents/solar/v8/data.yaml",
         epochs=100,
         imgsz=640,
     )
