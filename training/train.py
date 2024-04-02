59bec7c62f3dfe5dceaa18e8b7badf08f5b38504
Kai Glasenapp <91973727+MrZinken@users.noreply.github.com>
2024-04-02 14:05:04 +0200

    Update train.py

--- training/train.py
+++ training/train.py
@@ -1,14 +1,25 @@
 from ultralytics import YOLO
+import torch
 
-model = YOLO("yolov8l-seg.pt")
+if torch.cuda.is_available():
+    print("GPU is available")
+    training_device = "cuda"
+else:
+    print("GPU is not available")
+    training_device = "cpu"
+
+
+#model = YOLO("yolov8l-seg.pt")     
+model = YOLO("yolov9c-seg.pt")
 
 results = model.train(
         batch=13,
-        device="cuda",
+        device = training_device,
         data="/home/kai/Documents/solar/15/data.yaml",
         epochs=180,
         imgsz=640,
     )
+
 
 
 
